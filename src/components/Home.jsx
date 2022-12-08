8f3eb264a78aa10363480f05908bc5599301ee06
UgurcanSARICI <ugurcansarici@gmail.com>
2022-12-09 01:21:59 +0300

    photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import { slide1, slide2, slide3 } from "../assets/images";
 const Home = () => {
   return (
     <div>
-      <section className="relative bg-[url(http://localhost/b2b_task/assets/images/slide-3.png)] bg-cover bg-center bg-no-repeat">
+      <section className="relative bg-[url(https://i.ibb.co/2ZN3s2z/slide-3.png)] bg-cover bg-center bg-no-repeat">
         <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25" />
         <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
           <div className="max-w-xl text-center sm:text-left">
